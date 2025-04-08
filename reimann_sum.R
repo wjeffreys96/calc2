24853d4be26754535fcf7b4716d7316de8a9bbf6
= <=>
2025-04-08 16:44:17 -0700

    validate length of P, C in riemann_sum.R

--- reimann_sum.R
+++ reimann_sum.R
@@ -8,9 +8,13 @@ func <- function(x) {
 
 P <- c(0.2, 1.3, 2.3, 2.7, 3.8)
 
+# length (C) Must be length(P) - 1
 C <- c(0.3, 1.4, 2.5, 2.9)
 
 riemann_sum <- function(func, P, C) {
+  if (length(C) != (length(P) - 1)) {
+    stop("Length of P must be exactly one greater than length of C")
+  }
   delta_x <- diff(P)
   func_values <- sapply(C, func)
   rectangles <- func_values * delta_x
