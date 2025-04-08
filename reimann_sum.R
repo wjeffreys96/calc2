8e6b9c56b78df5a96c317513863954ccc80b6db2
= <=>
2025-04-08 16:39:47 -0700

    finish riemann_sum.R

--- reimann_sum.R
+++ reimann_sum.R
@@ -1,6 +1,7 @@
-# calculate the Reimann sum of a function given a partition P,
+# calculate the Riemann sum of a function given a partition P,
 # and a set of points C
 
+# test values
 func <- function(x) {
   x^2 + 4
 }
@@ -9,6 +10,12 @@ P <- c(0.2, 1.3, 2.3, 2.7, 3.8)
 
 C <- c(0.3, 1.4, 2.5, 2.9)
 
-print(c(func, P, C))
+riemann_sum <- function(func, P, C) {
+  delta_x <- diff(P)
+  func_values <- sapply(C, func)
+  rectangles <- func_values * delta_x
+  sum(rectangles)
+}
 
-# reimann_sum <- function(func, P, C) { }
+# test run
+print(riemann_sum(func, P, C))
